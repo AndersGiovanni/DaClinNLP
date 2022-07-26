807024f170cce728662bebf436c3c64699a67515
AGMoller <andersgiovanni@gmail.com>
2022-07-26 19:57:58 +0200

    add mail when job begins

--- runner.sh
+++ runner.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 #SBATCH --job-name=icd_baseline    # Job name
@@ -7,6 +6,6 @@
 #SBATCH --gres=gpu               # Schedule a GPU
 #SBATCH --time=05:00:00          # Run time (hh:mm:ss) - run for one hour max
 #SBATCH --partition=red    # Run on either the Red or Brown queue
-#SBATCH --mail-type=START,FAIL,END    # Send an email when the job finishes or fails
+#SBATCH --mail-type=BEGIN,FAIL,END    # Send an email when the job finishes or fails
 
 python src/baseline_.py
